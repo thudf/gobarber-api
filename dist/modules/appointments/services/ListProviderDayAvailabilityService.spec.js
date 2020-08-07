7a692625caf2f3ff874bb5b21f6caf23b1b2a0ad
Arthur <arthur.dafonseca89@gmail.com>
2020-08-06 21:14:33 -0300

    Dependabot correction

---
+++ dist/modules/appointments/services/ListProviderDayAvailabilityService.spec.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var _FakeAppointmentsRepository = _interopRequireDefault(require("../repositories/fakes/FakeAppointmentsRepository"));
+
+var _ListProviderDayAvailabilityService = _interopRequireDefault(require("./ListProviderDayAvailabilityService"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+let fakeAppointmentsRepository;
+let listProviderDayAvailability;
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new _FakeAppointmentsRepository.default();
+    listProviderDayAvailability = new _ListProviderDayAvailabilityService.default(fakeAppointmentsRepository);
+  });
+  it('should be able to list day availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      date: new Date(2020, 4, 20, 14, 0, 0),
+      provider_id: 'user',
+      user_id: 'user'
+    });
+    await fakeAppointmentsRepository.create({
+      date: new Date(2020, 4, 20, 15, 0, 0),
+      provider_id: 'user',
+      user_id: 'user'
+    });
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
+    });
+    const availability = await listProviderDayAvailability.execute({
+      provider_id: 'user',
+      day: 20,
+      month: 5,
+      year: 2020
+    });
+    expect(availability).toEqual(expect.arrayContaining([{
+      hour: 8,
+      available: false
+    }, {
+      hour: 9,
+      available: false
+    }, {
+      hour: 10,
+      available: false
+    }, {
+      hour: 13,
+      available: true
+    }, {
+      hour: 14,
+      available: false
+    }, {
+      hour: 15,
+      available: false
+    }, {
+      hour: 16,
+      available: true
+    }]));
+  });
+});
